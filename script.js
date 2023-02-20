126a2454f8170b64b33f0b80e391f1b3af3fb17c
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-20 15:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, index) => {
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (code.value === '') {
+        const prevCode = codes[index - 1];
+        if (prevCode) {
+          prevCode.focus();
+        }
+      }
+    }
+  });
+});
